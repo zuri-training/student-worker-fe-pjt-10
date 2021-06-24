33eaa1a8c13e7e13fc5d38f976498447e65ae014
zeerau <zeerabaidu@gmail.com>
2021-06-25 01:57:36 +0530

    nav commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const navSlide = ()=>{
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li')
+burger.addEventListener('click',() =>{
+nav.classList.toggle('active')
+navLinks.forEach((link,index) =>{
+if(link.style.animation){
+  link.style.animation = ''
+}else{
+  link.style.animation = `navlinkfade 0.5s ease forwards ${index / 7 + 1.5}s`
+}
+})
+
+burger.classList.toggle('toggle');
+})
+
+
+}
+
+navSlide();
