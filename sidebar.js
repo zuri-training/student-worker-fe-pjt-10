a14f54989f941793fe7642297081fb7a914c97fa
Abasiakara_Alexander <abasiakaraalexander@gmail.com>
2021-07-07 15:09:42 +0200

    new clone

---
+++ sidebar.js
@@ -0,0 +1,15 @@
+    var filtermobile = document.querySelector(".filterdrop")
+    var closebutton = document.querySelector(".closebtn2")
+    var sidee = document.querySelector(".sidebar")
+    function closeSidebar() {
+      sidee.style.display = "none";
+    }
+    function openSidebar(){
+        sidee.style.display = "block";
+    }
+    function filter(){
+      filtermobile.style.display = "block";
+    }
+    function closemodal(){
+      filtermobile.style.display = "none";
+    }
